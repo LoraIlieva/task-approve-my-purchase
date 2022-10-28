a9f930a9304894138f3653a6f001efbf3d648797
lora <lora.i.ilieva@gmail.com>
2022-10-28 15:31:14 +0300

    Implementation

--- src/handlers/President.java
+++ src/handlers/President.java
@@ -1,19 +1,31 @@
 package handlers;
 
 import common.Type;
+import java.security.InvalidParameterException;
 
-/**
- * //TODO - Implement approval implementation for President level
- */
+
 public class President extends Approver{
     @Override
     public void approve(int id, double cost, Type type) {
 
+        if (canApprove(cost, type)) {
+            System.out.println("President approved purchase with id " + id + " that costs " + cost);
+            return;
+        }
+
+        System.out.println("Purchase with id " + id + " needs approval from higher position than President.");
         next.approve(id, cost, type);
     }
 
     @Override
-    protected boolean canApprove(int id, double cost, Type type) {
-        return false;
+    protected boolean canApprove(double cost, Type type) {
+        if(cost <= 0) throw new InvalidParameterException();
+        return switch (type) {
+            case CONSUMABLES -> cost <= 1000;
+            case CLERICAL -> cost <= 2000;
+            case GADGETS -> cost <= 3000;
+            case GAMING -> cost <= 5000;
+            case PC -> cost <= 8000;
+        };
     }
 }
--- src/handlers/VicePresident.java
+++ src/handlers/VicePresident.java
@@ -1,20 +1,31 @@
 package handlers;
 
 import common.Type;
+import java.security.InvalidParameterException;
 
 
-/**
- * //TODO - Implement approval implementation for VicePresident level
- */
 public class VicePresident extends Approver{
     @Override
     public void approve(int id, double cost, Type type) {
 
+        if (canApprove(cost, type)) {
+            System.out.println("Vice president approved purchase with id " + id + " that costs " + cost);
+            return;
+        }
+
+        System.out.println("Purchase with id " + id + " needs approval from higher position than Vice president.");
         next.approve(id, cost, type);
     }
 
     @Override
-    protected boolean canApprove(int id, double cost, Type type) {
-        return false;
+    protected boolean canApprove(double cost, Type type) {
+        if(cost <= 0) throw new InvalidParameterException();
+        return switch (type) {
+            case CONSUMABLES -> cost <= 700;
+            case CLERICAL -> cost <= 1500;
+            case GADGETS -> cost <= 2000;
+            case GAMING -> cost <= 4500;
+            case PC -> cost <= 6500;
+        };
     }
 }
